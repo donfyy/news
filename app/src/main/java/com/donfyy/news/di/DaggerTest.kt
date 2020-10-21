82fd8fef6ef112304c603935fed116908eb594c9
donfyy <sven.xinxin.wang@gmail.com>
2020-10-21 14:46:49 +0800

    add more

--- app/src/main/java/com/donfyy/news/di/DaggerTest.kt
+++ app/src/main/java/com/donfyy/news/di/DaggerTest.kt
@@ -88,15 +88,27 @@ interface ITest2Service {}
 class Test2Service @Inject constructor() : ITest2Service
 
 @Singleton
-@Component(modules = [Test3Module::class])
+@Component(modules = [Test3Module::class, Test3Module2::class])
 interface Test3Component {
     fun inject(t: Test3)
+    fun getFoo(): Test3Foo
+    fun getModule2(): Test3Module2
+//    fun getTest3Module2():Test3Module2
+    @Component.Builder
+    interface Builder {
+//        @BindsInstance
+//        fun foo(f: Test3Foo): Builder
+        fun module2(m: Test3Module2): Builder
+        fun build(): Test3Component
+    }
 }
 
 class Test3 {
 
     @Inject
     lateinit var t2: ITest3Service
+//    @Inject
+//    lateinit var b: Test3Bar
     // 通过@Inject提供的类，在依赖处不能使用接口，只能使用类名
     // 如果使用接口，需要在Module中提供
 //    @Inject lateinit var t2 : Test3Service
@@ -107,7 +119,24 @@ abstract class Test3Module {
     // ITest3Service的实例将有Test3Service来提供
     @Binds
     abstract fun provideITest3Service(s: Test3Service): ITest3Service
+    companion object {
+        @Provides
+        fun provideFoo(b:Test3Bar): Test3Foo {
+            return Test3Foo(b)
+        }
+    }
 }
+
+@Module
+class Test3Module2 {
+    @Provides
+    fun provideModule2() = this
+    @Provides
+    fun provideTest3Bar() = Test3Bar()
+}
+
+class Test3Foo(b: Test3Bar)
+class Test3Bar
 
 interface ITest3Service {}
 
